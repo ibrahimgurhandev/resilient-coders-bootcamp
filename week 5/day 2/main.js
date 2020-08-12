da758894bece360f31cdc7bf4390abf7ed8e7a41
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-08-11 21:07:01 -0400

    did the assignment in another way

--- week 5/day 2/main.js
+++ week 5/day 2/main.js
@@ -6,8 +6,101 @@ var box3 = document.getElementById('box3');
 var clickbox1 = 0;
 var clickbox2 = 0;
 var clickbox3 = 0;
+var numDeg= 0;
+
+// box2.addEventListener("click",rotateBox2)
+// box3.addEventListener("click",rotateBox3)
+
+// Leons Method
+box1.addEventListener("click", ()=>{
+  numDeg +=45;
+  box1.style.transform = `rotateZ(${numDeg}deg)`
+  clickbox1++;
+if (clickbox1===1){
+  box1.style.borderRadius = "50%";
+  box1.style.marginLeft = "10%";
+}else if (clickbox1 == 2){
+  box1.style.borderRadius = "0%";
+  box1.style.marginLeft = "80px";
+  }else if (clickbox1 % 3 == 0 ){
+    box2.style.backgroundColor = ('green');
+    box3.style.backgroundColor = ('green');
+  }else {
+    box2.style.backgroundColor = ('white');
+    box3.style.backgroundColor = ('white');
+  }
+})
 
 
+
+
+
+
+
+
+
+box2.addEventListener("click", ()=>{
+  numDeg +=45;
+  box2.style.transform = `rotateZ(${numDeg}deg)`
+  clickbox2++;
+if (clickbox2===1){
+  box2.style.borderRadius = "50%";
+  box2.style.marginLeft = "10%";
+}else if (clickbox2 == 2){
+  box2.style.borderRadius = "0%";
+  box2.style.marginLeft = "80px";
+}else if (clickbox2 % 3 == 0 ){
+    box1.style.backgroundColor = ('red');
+    box3.style.backgroundColor = ('red');
+  }else {
+    box1.style.backgroundColor = ('white');
+    box3.style.backgroundColor = ('white');
+  }
+})
+
+
+// Leons Method
+box3.addEventListener("click", ()=>{
+  numDeg +=45;
+  box3.style.transform = `rotateZ(${numDeg}deg)`
+  clickbox3++;
+if (clickbox3===1){
+  box3.style.borderRadius = "50%";
+  box3.style.marginLeft = "10%";
+}else if (clickbox3 == 2){
+  box3.style.borderRadius = "0%";
+  box3.style.marginLeft = "80px";
+}else if (clickbox3 % 3 == 0 ){
+    box1.style.backgroundColor = ('purple');
+    box2.style.backgroundColor = ('purple');
+  }else {
+    box1.style.backgroundColor = ('white');
+    box2.style.backgroundColor = ('white');
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+box2.addEventListener("click",rotateBox2)
+box3.addEventListener("click",rotateBox3)
+
+
+
+
+//First Method
 
 function rotateBox1(){
 
@@ -58,9 +151,3 @@ function rotateBox3(){
     box2.classList.toggle('purple');
   }
 }
-
-
-
-box1.addEventListener("click",rotateBox1)
-box2.addEventListener("click",rotateBox2)
-box3.addEventListener("click",rotateBox3)
