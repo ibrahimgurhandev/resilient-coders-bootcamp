9d21ec0d40558d1011e191392649f9fb06119a81
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2021-03-25 18:01:31 -0400

    update

--- Week4/calculator/js/main.js
+++ Week4/calculator/js/main.js
@@ -1,69 +1,34 @@
-// my pseudo code:
-// first thing I need to do is assign a variable to all the
-// calc keys and pull them from the html
-//
-// I will do the same thing for my display screen input,
-// and the clear and equal button will have their
-// variables assigned to them
-//
-// Then create a loop that will pull all the number keys
-//  passed into aa loop when a number is pressed it will
-//   equal what ever it's id is using the get attribute mehtod.
-// then that key pressed will be added to the display screen
-//
-// the clear button should be easy if I just make function
-// that will make the display screen empty
-//
-// The calculations will be done using the builtin javascript method called eval
-
-
-var allNumKeys = document.getElementsByClassName('btn');
+var allNumKeys = [...document.getElementsByClassName('btn')]
 var displayScreen = document.getElementById("displayScreen");
 var equalBtn = document.getElementById("equals");
 const clear = document.getElementById("clear");
 var del = document.getElementById("del");
 
 
-// Loop through all buttons with a for loop
-//basically this loop will add event listener to all the buttons
-for (let i = 0; i < allNumKeys.length; i++) {
-allNumKeys[i].addEventListener("click",pullTheKeys)
+allNumKeys.forEach(btn => btn.addEventListener("click", ()=>{ 
+  checkInput()
+  console.log(checkInput())
+  displayScreen.value += btn.getAttribute("id")
 
-function pullTheKeys() {
-  // Assign the attribute of each button to the variable number
-  let number = allNumKeys[i].getAttribute("id");
-  // Add the attributes of number to the screen value
-  displayScreen.value += number;
-}
-}
+}))
 
 
-// An if condition is used so that it only evaluates if there are values ​​on the screen.
-equalBtn.addEventListener("click",theCalculation)
-
-function theCalculation() {
-if (displayScreen.value === "") {
-  alert("Enter a Math Problem to continue");
-} else {
-  // using this javascript property eval after watching youtube tutorial. does the calculations for you.
-  let solution = eval(displayScreen.value);
-  displayScreen.value = solution;
-}
+function checkInput(){
+  return displayScreen.value.match("^\d+(\.\d+)?([+\-*\/]\d+(\.\d+)?)*$")
 }
 
-clear.addEventListener("click", clearDisplay)
+equalBtn.addEventListener("click", ()=>  {
+  if (operands.indexOf(displayScreen.value.slice(-1)) !== -1) {
+    alert("Enter a Math problem correctly to continue");
+  } else {
+    // using this javascript property eval after watching youtube tutorial. does the calculations for you.
+    let solution = eval(displayScreen.value);
+    displayScreen.value = solution;
+    }
+  }
+)
 
-// This function will change the screen value to "empty" when the clear button is clicked
+clear.addEventListener("click", () => displayScreen.value = "")
 
-function clearDisplay() {
-  // reset the display screen
-displayScreen.value = "";
-}
+del.addEventListener("click", ()=> displayScreen.value = displayScreen.value.slice(0, -1))
 
-
-function delDisplay() {
-  // reset the display screen
-displayScreen.value = displayScreen.value.slice(0,-1);
-}
-
-del.addEventListener("click", delDisplay)
